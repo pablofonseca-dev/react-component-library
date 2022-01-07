a4585944f27a47def3ba6e65fb07233a8cdf4b3c
Pablo_Fonseca <pablo.fonseca@firstfactory.com>
2022-01-06 19:25:50 -0600

    [COMPLIB-07] Prettier, ESLint and Husky hooks configured, also added some architecture packages.

---
+++ src/models/IButton/IButton.interface.ts
@@ -0,0 +1,14 @@
+type StyleType =
+  | "primary"
+  | "secondary"
+  | "success"
+  | "danger"
+  | "warning"
+  | "info"
+  | "light"
+  | "dark";
+
+interface IButton {
+  style: StyleType;
+  variant: string;
+}
