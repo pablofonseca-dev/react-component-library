ee03082e914bbc56527c8f8e03d1c2a7e0c66c2d
Pablo Fonseca <Pablo.fonsecam@outlook.com>
2022-01-14 16:44:58 -0600

    [COMPLIB-26] Some files cleaned with prettier

--- src/TestingPage.tsx
+++ src/TestingPage.tsx
@@ -1,10 +1,27 @@
-import React from "react";
-import Button from "./components/Button/Button";
+import { createRef } from "react";
+import Input from "./components/Input/Input";
 
 export default function TestingPage() {
+    const handleChange = (e: any) => {
+        e.preventDefault();
+        console.log("Hello");
+        console.log(e.target.value);
+    };
+    const inputRef = createRef<HTMLInputElement>();
+
+    const clickHandler = () => {
+        inputRef!.current!.focus();
+    };
+
     return (
         <div>
-            <Button>Hello World</Button>
+            <Input
+                inputSize={"large"}
+                rounded={true}
+                isError={true}
+                placeholder="john@example.com"
+            />
+            <button onClick={clickHandler}>Click & Focus!</button>
         </div>
     );
 }
