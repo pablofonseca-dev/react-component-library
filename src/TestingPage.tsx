4aed29268da6ba180182e8ed602ba604e968e978
Pablo Fonseca <Pablo.fonsecam@outlook.com>
2022-01-14 12:24:09 -0600

    [COMPLIB-21] Unnecessary files from [COMPLIB-08] reversion deleted, some changes made to prepare testing playground.

---
+++ src/TestingPage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Button from "./components/Button/Button";
+
+export default function TestingPage() {
+    return (
+        <div>
+            <Button>Hello World</Button>
+        </div>
+    );
+}
